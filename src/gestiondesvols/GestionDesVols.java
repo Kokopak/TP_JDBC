93c5ca22410a0e24b22c2c922cd23d2da016bc96
Ilona_p <ilona.pradel@etud.univ-jfc.fr>
2017-12-19 15:48:52 +0100

    Initialisation

---
+++ src/gestiondesvols/GestionDesVols.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestiondesvols;
+import java.sql.*;
+
+/**
+ *
+ * @author ipradel
+ */
+public class GestionDesVols {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            Class.forName("oracle.jdbc.driver.ojdbc7.jar");
+            System.out.println("Pilote chargé");
+        } catch (java.lang.ClassNotFoundException e) {
+            System.err.println("Erreur : " + e.getMessage());
+        }
+    }
+    
+}
