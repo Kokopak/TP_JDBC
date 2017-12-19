d1503b1883bf34589f6d605b176dcd70581631ce
cchataig <cchataig@10.183.102.2>
2017-12-19 16:16:46 +0100

    Connexion pour la base de données

--- src/gestiondesvols/GestionDesVols.java
+++ src/gestiondesvols/GestionDesVols.java
@@ -10,17 +10,36 @@ import java.sql.*;
  *
  * @author ipradel
  */
+
+
 public class GestionDesVols {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static Connection conn = null;
+
     public static void main(String[] args) {
+  
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             System.out.println("Pilote chargé");
         } catch (java.lang.ClassNotFoundException e) {
             System.err.println("Erreur : " + e.getMessage());
+        }
+        
+        try {
+            String url = "jdbc:oracle:thin:@licinfo.univ-jfc.fr:1521:pedago";
+            String user = "ISIS3_20";
+            String password = "isis";
+            
+            conn = DriverManager.getConnection(url, user, password);
+            
+            if(conn != null) {
+                System.out.println("Connexion établie !");
+            }
+            else {
+                System.out.println("Echec de la connexion");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
         }
     }
     
